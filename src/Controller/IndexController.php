0bf36987db9c36e807a9e2e84685233196491579
Hernán Molinari <40526668+pidurffi@users.noreply.github.com>
2021-01-26 08:36:53 -0300

    TERMINANDO EL crud

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\MarcadorRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,12 +10,13 @@ use Symfony\Component\Routing\Annotation\Route;
 class IndexController extends AbstractController
 {
     /**
-     * @Route("/panel", name="app_index")
+     * @Route("/", name="app_index")
      */
-    public function index(): Response
+    public function index(MarcadorRepository $marcadorRepository)
     {
+        $marcadores = $marcadorRepository->findAll();
         return $this->render('index/index.html.twig', [
-            'controller_name' => 'CategoriaController',
+            'marcadores' => $marcadores,
         ]);
     }
 }
--- src/Entity/Categoria.php
+++ src/Entity/Categoria.php
@@ -55,4 +55,9 @@ class Categoria
 
         return $this;
     }
+
+    public function __toString()
+    {
+        return $this->nombre;
+    }
 }
