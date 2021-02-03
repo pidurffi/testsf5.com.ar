2ee12775e519e55ac9e979de84f38bd619c543ac
Hernán Molinari <hmolinari@gmail.com>
2021-02-03 20:02:23 -0300

    Quedó andando el ajax !!!

---
+++ public/js/functions.js
@@ -0,0 +1,20 @@
+$('.favorito').on('click', function(e) {
+    e.preventDefault();
+    var $this = $(this),
+        url = $this.data('url'),
+        idMarcador = $this.data('id');  
+
+    $this.addClass('disabled');
+  
+
+        $.post(url, {id: idMarcador})
+        .done(function(respuesta) {
+            if (respuesta.actualizado){
+                $this.toggleClass('activo');
+            }
+            $this.removeClass('disabled');
+
+        }).fail(function(){
+            $this.removeClass('disabled');
+        });
+});
--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -5,12 +5,37 @@ namespace App\Controller;
 use App\Repository\CategoriaRepository;
 use App\Repository\MarcadorRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class IndexController extends AbstractController
 {
-    
+    /**
+     * @route("/editar-favorito", name="app_editar_favorito" )
+     */
+    public function editarFavorito(MarcadorRepository $marcadorRepository, Request $request)
+    {
+        if ($request->isXmlHttpRequest()){
+            $idMarcador = $request->get('id');
+            $entityManager = $this->getDoctrine()->getManager();
+            $actualizado = true;
+            //busco el mrcador por id
+            $marcador = $marcadorRepository->findOneById($idMarcador);
+            $marcador->setFavorito(!$marcador->getFavorito());
+
+            try {
+                $entityManager->flush();
+            } catch(\Exception $e) {
+                $actualizado = false;
+            }
+            return $this->json([
+                'actualizado' => $actualizado
+            ]);
+        }
+        throw $this->createNotFoundException();
+    }
+
     /**
      * @route("/favoritos", name="app_favoritos" )
      */
